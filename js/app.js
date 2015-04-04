a361e2fa4d8412b8ef52c6c747b1c393fd224ffa
Lyndi Holzwarth <lyndi.holzwarth@gmail.com>
2015-04-04 14:16:57 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	for (var myNumber = 1; myNumber < 100; myNumber++) {
+   document.write(myNumber+"<br> ");
+   if ((myNumber/3) === true){
+   	document.write(fizz)
+   }
+}
+
+
+});
