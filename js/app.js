9c21e8b1173f2363b5ec2a4e6fa2821a99f61e88
Lyndi Holzwarth <lyndi.holzwarth@gmail.com>
2015-04-05 13:56:00 -0400

    buzz and fizz work seperately

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
-	for (var myNumber = 1; myNumber < 100; myNumber++) {
-   document.write(myNumber+"<br> ");
-   if ((myNumber/3) === true){
-   	document.write(fizz)
-   }
-}
-
+	for (var myNumber = 1; myNumber < 100; myNumber++) 
+	   if (((myNumber/3)%1)===0){
+	   	document.write("fizz" +"<br>");
+	   }
+	   else if (((myNumber/5)%1)===0){
+	   	document.write("buzz" +"<br>");
+	   }
+	   else if (((myNumber/3)%1===0)==((myNumber/5)%1)===0){
+	   	document.write("fizz-buzz"+"<br>");
+	   }
+	   else {
+	   	document.write(myNumber+"<br> ");
+	    } ;  
 
 });
