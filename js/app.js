90d2395a80c749bb07e5f104bbafd074efa4ef7f
Lyndi Holzwarth <lyndi.holzwarth@gmail.com>
2015-04-05 14:08:40 -0400

    working and finished

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
 	for (var myNumber = 1; myNumber < 100; myNumber++) 
-	   if (((myNumber/3)%1)===0){
+	   if (((myNumber/3)%1===0)&&((myNumber/5)%1)===0){
+	   	document.write("fizz-buzz"+"<br>");
+	   }
+	   	else if (((myNumber/3)%1)===0){
 	   	document.write("fizz" +"<br>");
 	   }
 	   else if (((myNumber/5)%1)===0){
 	   	document.write("buzz" +"<br>");
-	   }
-	   else if (((myNumber/3)%1===0)==((myNumber/5)%1)===0){
-	   	document.write("fizz-buzz"+"<br>");
 	   }
 	   else {
 	   	document.write(myNumber+"<br> ");
