2c9955a4f36e92ffbd0cafaa74d72bcddcada047
Lyndi Holzwarth <lyndi.holzwarth@gmail.com>
2015-04-07 21:00:29 -0400

    refacter update

--- js/app.js
+++ js/app.js
@@ -1,16 +1,35 @@
 $(document).ready(function(){
-	for (var myNumber = 1; myNumber < 100; myNumber++) 
-	   if (((myNumber/3)%1===0)&&((myNumber/5)%1)===0){
-	   	document.write("fizzbuzz"+"<br>");
-	   }
-	   	else if (((myNumber/3)%1)===0){
-	   	document.write("fizz" +"<br>");
-	   }
-	   else if (((myNumber/5)%1)===0){
-	   	document.write("buzz" +"<br>");
-	   }
-	   else {
-	   	document.write(myNumber+"<br> ");
-	    } ;  
+	var max = prompt("How high should I FizzBuzz? Please supply a number");
+	console.log(Number(max))
+	
+	//check if number//
+	if (isNaN(max)==true){
+		console.log("not a number")
+		alert("Sorry, I need a real number.")
+		var max = prompt("Please try again. How high should I FizzBuzz?")
+	}
 
+	//test for decimal//
+	if (max % 1 != 0){
+		console.log("decimal")
+		alert("Sorry, I need a whole number.")
+		var max = prompt("Please try again. How high should I FizzBuzz?")
+	}
+
+	//run fizzbuzz//
+	else if (max % 1 ==0){
+		for (var myNumber = 1; myNumber <=max; myNumber++) 
+		   if (((myNumber%3)===0)&&((myNumber%5)===0)){
+		   	document.write("fizzbuzz"+"<br>");
+		   }
+		   	else if ((myNumber%3)===0){
+		   	document.write("fizz" +"<br>");
+		   }
+		   else if ((myNumber%5)===0){
+		   	document.write("buzz" +"<br>");
+		   }
+		   else {
+		   	document.write(myNumber+"<br> ");
+		    } ;  
+	}
 });
